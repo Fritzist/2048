2cedec829b2c5dbbe6a5ced4c4dd212b85559aa4
Fritzist <danielhartl856@gmail.com>
2022-05-19 19:03:45 +0200

    swipe test

--- app.js
+++ app.js
@@ -136,3 +136,30 @@ function slideTiles(cells) {
       })
     })
   }
+
+  let startingX, startingY, endingX, endingY;
+let moving = false;
+function touchstart(evt) {
+    startingX = evt.touches[0].clientX;
+    startingY = evt.touches[0].clientY;
+}
+function touchmove(evt) {
+    moving = true;
+    endingX = evt.touches[0].clientX;
+    endingY = evt.touches[0].clientY;
+}
+function touchend() {
+    if (!moving || block) return;
+    let touchDirection;
+    if ( Math.abs(endingX - startingX) > Math.abs(endingY - startingY) ) {
+        if ( endingX > startingX ) touchDirection = "ArrowRight";
+        else touchDirection = "ArrowLeft";
+        console.log("left")
+        
+    } else {
+        if ( endingY > startingY ) touchDirection = "ArrowDown";
+        else touchDirection = "ArrowUp";
+    }
+    handleInteraction(touchDirection);
+    moving = false;
+}
